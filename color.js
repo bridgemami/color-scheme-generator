f28a2e6a74b8c5187ac15dbfca37ee93cc01d0ea
Michael <mdbridgeman@gmail.com>
2023-09-09 17:01:20 -0700

    created a default scheme to start

--- color.js
+++ color.js
@@ -1,11 +1,40 @@
-export const colorPicker = function (value, scheme, count = 5) {
+function fetchColors (value, scheme, count=5){
+  return fetch(
+        `https://www.thecolorapi.com/scheme?hex=${value}&format=json&mode=${scheme}&count=${count}`
+      )
+}
+
+const renderColors = async function (value, scheme, count = 5) {
   try {
-    fetch(
-      `https://www.thecolorapi.com/scheme?hex=${value}&format=json&mode=${scheme}&count=${count}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const schemeEl = document.getElementById("scheme");
+    // fetchColors(value, scheme, count)
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     const schemeEl = document.getElementById("scheme");
+    //     const copyEl = document.getElementById("copy");
+    //     let html = ``;
+    //     let schemeArr = [];
+    //     data.colors.map((color) => {
+    //       schemeArr.push(color.hex.value);
+    //       html += `<div class="col-color">
+    //             <img src="${color.image.bare}" alt="${color.name.closest_named_hex}" class="img-color" />
+    //             <p class='hex-color'>${color.hex.value}</p>
+    //             <p class='name-color'>${color.name.value}</p>
+    //             </div>`;
+    //     });
+    //     schemeArr.join("");
+    //     schemeEl.innerHTML = html;
+    //     copyEl.innerHTML =
+    //       "<button id='btn-copy' class='btn-copy'>Click to copy color scheme</button>";
+    //     const copyBtn = document.getElementById("btn-copy");
+    //     copyBtn.addEventListener("click", () => {
+    //       console.log(schemeArr)
+    //       navigator.clipboard.writeText('yippe');
+    //       copyBtn.textContent = "Copied";
+    //     });
+    //   });
+    const res = await fetchColors(value, scheme, count)
+    const data = await res.json()
+    const schemeEl = document.getElementById("scheme");
         const copyEl = document.getElementById("copy");
         let html = ``;
         let schemeArr = [];
@@ -25,10 +54,11 @@ export const colorPicker = function (value, scheme, count = 5) {
         copyBtn.addEventListener("click", () => {
           navigator.clipboard.writeText(schemeArr);
           copyBtn.textContent = "Copied";
-        });
-      });
+        }); 
   } 
   catch {
     (err) => console.error(err);
   }
 };
+
+export default renderColors
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { colorPicker } from './color.js';
+import  renderColors  from './color.js';
 
 const formEl = document.getElementById('form');
 const colorInput = document.getElementById('color');
@@ -6,22 +6,27 @@ const schemeInput = document.getElementById('color-scheme')
 const schemeEl = document.getElementById('scheme');
 const copyEl = document.getElementById("copy");
 
+checkSchemeInput()
+
+function checkSchemeInput(){
+  switch(schemeInput.value){
+    case 'triad':
+      renderColors(colorInput.value.slice(1), schemeInput.value, 3);
+    case 'quad':
+      renderColors(colorInput.value.slice(1), schemeInput.value, 4);
+    case '0':
+      schemeEl.innerHTML = '<h2>Please select a scheme</h2>';
+    default:
+      renderColors(colorInput.value.slice(1), schemeInput.value);
+  }
+}
 
 formEl.addEventListener('submit', (e) => {
     e.preventDefault();
-  if(schemeInput.value === 'triad') {
-    colorPicker(colorInput.value.slice(1), schemeInput.value, 3);
-  }
-  else if (schemeInput.value === 'quad') {
-    colorPicker(colorInput.value.slice(1), schemeInput.value, 4);
-  }
-  else if (schemeInput.value === '0') {
-      schemeEl.innerHTML = '<h2>Please select a scheme</h2>';
-  }
-  else {
-    colorPicker(colorInput.value.slice(1), schemeInput.value);
-  }
+    checkSchemeInput()
 })
+
+
 
 formEl.addEventListener('reset', () => {
   formEl.reset();
