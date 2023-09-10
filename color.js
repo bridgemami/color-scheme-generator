43bfb059837619a2e1e40dafd4733b1ae6ccb5da
Michael <mdbridgeman@gmail.com>
2023-09-09 17:01:51 -0700

    deleted my notes

--- color.js
+++ color.js
@@ -6,32 +6,6 @@ function fetchColors (value, scheme, count=5){
 
 const renderColors = async function (value, scheme, count = 5) {
   try {
-    // fetchColors(value, scheme, count)
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     const schemeEl = document.getElementById("scheme");
-    //     const copyEl = document.getElementById("copy");
-    //     let html = ``;
-    //     let schemeArr = [];
-    //     data.colors.map((color) => {
-    //       schemeArr.push(color.hex.value);
-    //       html += `<div class="col-color">
-    //             <img src="${color.image.bare}" alt="${color.name.closest_named_hex}" class="img-color" />
-    //             <p class='hex-color'>${color.hex.value}</p>
-    //             <p class='name-color'>${color.name.value}</p>
-    //             </div>`;
-    //     });
-    //     schemeArr.join("");
-    //     schemeEl.innerHTML = html;
-    //     copyEl.innerHTML =
-    //       "<button id='btn-copy' class='btn-copy'>Click to copy color scheme</button>";
-    //     const copyBtn = document.getElementById("btn-copy");
-    //     copyBtn.addEventListener("click", () => {
-    //       console.log(schemeArr)
-    //       navigator.clipboard.writeText('yippe');
-    //       copyBtn.textContent = "Copied";
-    //     });
-    //   });
     const res = await fetchColors(value, scheme, count)
     const data = await res.json()
     const schemeEl = document.getElementById("scheme");
